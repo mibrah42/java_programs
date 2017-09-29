561d451a8513dff36bee935e2bd766b5a570dc0d
mibrah42 <mohamed.ibrahim1@uoit.net>
2017-09-28 20:31:53 -0400

    removed javadoc

--- Rectangle/Point.java
+++ Rectangle/Point.java
@@ -15,6 +15,11 @@ public class Point {
 		}
 	}
 
+	// public void add(Point p) {
+	// 	this.x += p.x;
+	// 	this.y += p.y;
+	// }
+
 	public static void main(String[] args) {
 		Point p1 = new Point(5, 7);
 		Point p2 = new Point(5, 7);
--- bank_account/BankAccount.java
+++ bank_account/BankAccount.java
@@ -1,16 +1,53 @@
 public class BankAccount{
+
 	private double balance;
 
-	public void deposit(double amount){
-		balance += amount;
+
+	public BankAccount(){
+		balance = 0.0;
 	}
 
-	public void withdraw(double amount){
-		balance -= amount;
+	public BankAccount(double balance) {
+		this.balance = balance;
 	}
 
 	public double getBalance(){
 		return balance;
 	}
+
+	public void deposit(double amount){
+		this.balance += amount;
+	}
+
+	public void withdraw(double amount){
+		this.balance -= amount;
+	}
+
+
+	public static void main(String[] args) {
+		BankAccount mike = new BankAccount();
+		System.out.println(mike.getBalance());
+		BankAccount sarah = new BankAccount(100.0);
+		sarah.deposit(25.0);
+		sarah.withdraw(12.0);
+		System.out.println(sarah.getBalance());
+		BankAccount emily = sarah;
+		System.out.println(emily.getBalance());
+	}
+
+
+	// private double balance;
+
+	// public void deposit(double amount){
+	// 	balance += amount;
+	// }
+
+	// public void withdraw(double amount){
+	// 	balance -= amount;
+	// }
+
+	// public double getBalance(){
+	// 	return balance;
+	// }
 }
 
